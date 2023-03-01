c6f2bd6373fac56faa7935af95a246cf705f73fa
Daniel-gallegO <danielgallegolopez@outlook.com>
2023-03-01 15:01:35 +0100

    1-Conceptos: Palabras reservadas

---
+++ src/com/company/A06_wordsReserv.java
@@ -0,0 +1,29 @@
+package com.company;
+
+public class A06_wordsReserv {
+
+    // PALABRAS RESERVADAS
+
+    /**
+     * java kyewords
+     * https://docs.oracle.com/javase/tutorial/java/nutsandbolts/_keywords.html
+     * https://www.w3schools.com/java/java_ref_keywords.asp
+     *
+     * abstract	continue	for	new	switch
+     * assert***	default	goto*	package	synchronized
+     * boolean	do	if	private	this
+     * break	double	implements	protected	throw
+     * byte	else	import	public	throws
+     * case	enum****	instanceof	return	transient
+     * catch	extends	int	short	try
+     * char	final	interface	static	void
+     * class	finally	long	strictfp**	volatile
+     * const*	float	native	super	while
+     *
+     * En general las palabras reservadas aparecen en tono de azul
+     * Recordar que las variables se escriben con la primer letra en MINÚSCULAS
+     * y las clases se escriben con la primer letra en MAYÚSCULAS
+     */
+
+
+}
