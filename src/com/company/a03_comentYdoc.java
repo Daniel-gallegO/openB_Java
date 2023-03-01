0eae026c4f1f094427c064f7d2cb46e7e4a3f178
Daniel-gallegO <danielgallegolopez@outlook.com>
2023-03-01 11:40:21 +0100

    1-Conceptos Comemtarios y documentacion

---
+++ src/com/company/a03_comentYdoc.java
@@ -0,0 +1,38 @@
+package com.company;
+
+/**
+ * Clase principal, punto de entrada aplicación
+ * @author  Daniel-gallegO
+ * @see otras clases o datos
+ */
+public class a03_comentYdoc {
+
+    /**
+     *Función principal, punto de entrada a la aplicación
+     * @param args << argumentos a recibir
+     *
+     * @since 1.6
+     *
+     *
+     */
+    public static void main(String[] args) {
+
+       // write your code here
+
+        /* Aqui un texto de comentario */
+
+        /*
+
+        1. Abrir fichero
+        2. Leer fichero
+        3. Procesar un fichero
+        4. Cerrar fichero
+
+
+         */
+
+        // imprimir texto
+
+        System.out.println("Hola Luna"); // imprimir texto
+    }
+}
